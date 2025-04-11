f74d583d7ac81f5a52a56fc80207fe950b2757d0
maaaaster <dodopizza24@outlook.com>
2025-04-11 11:04:06 +0300

    sliders now work

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import sys
 import cv2
+import numpy as np
 from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6.QtWidgets import QFileDialog, QMessageBox
 from PyQt6.QtGui import QImage, QPixmap
@@ -9,7 +10,7 @@ class Ui_Video_Processor:
     def __init__(self, processor):
         self.processor = processor
         self.sharpness_value = 0
-        self.contrast_value = 0
+        self.contrast_value = 1
         self.brightness_value = 0
         self.noise_value = 0
         self.video_path = None
@@ -17,6 +18,7 @@ class Ui_Video_Processor:
     def setupUi(self, Video_Processor):
         Video_Processor.setObjectName("Video_Processor")
         Video_Processor.resize(800, 600)
+        Video_Processor.move(100, 100)
         Video_Processor.setStyleSheet("background-color: rgb(216, 250, 255); color: black;")
 
         button_style_red = """
@@ -66,16 +68,10 @@ class Ui_Video_Processor:
         self.frame.setScaledContents(True)
 
         self.mask = QtWidgets.QLabel(parent=self.centralwidget)
-        self.mask.setGeometry(QtCore.QRect(10, 310, 250, 200))
+        self.mask.setGeometry(QtCore.QRect(10, 310, 521, 250))
         self.mask.setFrameShape(QtWidgets.QFrame.Shape.Box)
         self.mask.setObjectName("mask")
         self.mask.setScaledContents(True)
-
-        self.video_scroll = QtWidgets.QSlider(parent=self.centralwidget)
-        self.video_scroll.setGeometry(QtCore.QRect(10, 560, 771, 20))
-        self.video_scroll.setOrientation(QtCore.Qt.Orientation.Horizontal)
-        self.video_scroll.setObjectName("video_scroll")
-        self.video_scroll.valueChanged.connect(self.update_frame_by_slider)
 
         self.brt_slide = QtWidgets.QSlider(parent=self.centralwidget)
         self.brt_slide.setGeometry(QtCore.QRect(550, 80, 160, 16))
@@ -92,10 +88,6 @@ class Ui_Video_Processor:
         self.shrrp_slide.setOrientation(QtCore.Qt.Orientation.Horizontal)
         self.shrrp_slide.setObjectName("shrrp_slide")
 
-        self.graph = QtWidgets.QFrame(parent=self.centralwidget)
-        self.graph.setGeometry(QtCore.QRect(280, 310, 250, 200))
-        self.graph.setFrameShape(QtWidgets.QFrame.Shape.StyledPanel)
-        self.graph.setObjectName("graph")
 
         self.start = QtWidgets.QPushButton(parent=self.centralwidget)
         self.start.setGeometry(QtCore.QRect(550, 350, 221, 41))
@@ -127,12 +119,12 @@ class Ui_Video_Processor:
         self.label_8.setGeometry(QtCore.QRect(550, 160, 101, 16))
         self.label_8.setObjectName("label_8")
 
-        self.noise_check = QtWidgets.QCheckBox(parent=self.centralwidget)
-        self.noise_check.setGeometry(QtCore.QRect(550, 210, 101, 17))
-        self.noise_check.setObjectName("noise_check")
+        self.label_9 = QtWidgets.QLabel(parent=self.centralwidget)
+        self.label_9.setGeometry(QtCore.QRect(550, 210, 101, 16))
+        self.label_9.setObjectName("label_6")
 
         self.noise_slide = QtWidgets.QSlider(parent=self.centralwidget)
-        self.noise_slide.setGeometry(QtCore.QRect(550, 240, 160, 20))
+        self.noise_slide.setGeometry(QtCore.QRect(550, 230, 160, 20))
         self.noise_slide.setOrientation(QtCore.Qt.Orientation.Horizontal)
         self.noise_slide.setObjectName("noise_slide")
 
@@ -158,7 +150,7 @@ class Ui_Video_Processor:
         self.label_4.setObjectName("label_4")
 
         self.video_time = QtWidgets.QLabel(parent=self.centralwidget)
-        self.video_time.setGeometry(QtCore.QRect(380, 530, 100, 13))
+        self.video_time.setGeometry(QtCore.QRect(620, 530, 100, 13))
         self.video_time.setObjectName("video_time")
 
         Video_Processor.setCentralWidget(self.centralwidget)
@@ -172,7 +164,7 @@ class Ui_Video_Processor:
 
         self.cntr_slide.setValue(1)
 
-        self.brt_slide.setRange(-255, 255)
+        self.brt_slide.setRange(-100, 100)
         self.cntr_slide.setRange(-15, 15)
         self.shrrp_slide.setRange(0, 100)
         self.noise_slide.setRange(0, 100)
@@ -183,6 +175,7 @@ class Ui_Video_Processor:
         self.total_frames = 0
         self.current_frame_index = 0
         self.timer = QtCore.QTimer()
+        self.set_default_slider_values()
         # self.timer.timeout.connect(self.play_video_frame)
 
     def retranslateUi(self, Video_Processor):
@@ -195,7 +188,7 @@ class Ui_Video_Processor:
         self.label_6.setText(_translate("Video_Processor", "Яркость"))
         self.label_7.setText(_translate("Video_Processor", "Контрастность"))
         self.label_8.setText(_translate("Video_Processor", "Резкость"))
-        self.noise_check.setText(_translate("Video_Processor", "Удалить шум"))
+        self.label_9.setText(_translate("Video_Processor", "Шум"))
         self.noise.setText(_translate("Video_Processor", "0"))
         self.default_btn.setText(_translate("Video_Processor", "По умолчанию"))
         self.file.setText(_translate("Video_Processor", "ВЫБРАТЬ ФАЙЛ"))
@@ -223,7 +216,6 @@ class Ui_Video_Processor:
 
             self.cap = cv2.VideoCapture(file_path)
             self.total_frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
-            self.video_scroll.setMaximum(self.total_frames - 1)
             self.current_frame_index = 0
             self.show_frame(self.current_frame_index)
             self.file.setText("Файл выбран")
@@ -255,15 +247,9 @@ class Ui_Video_Processor:
 
         self.video_time.setText(f"{format_time(current_seconds)} / {format_time(total_seconds)}")
 
-    def update_frame_by_slider(self):
-        index = self.video_scroll.value()
-        self.current_frame_index = index
-        self.show_frame(index)
-
     def play_video_frame(self):
         if self.cap and self.cap.isOpened() and self.current_frame_index < self.total_frames:
             self.show_frame(self.current_frame_index)
-            self.video_scroll.setValue(self.current_frame_index)
             self.current_frame_index += 1
         else:
             self.timer.stop()
@@ -278,7 +264,7 @@ class Ui_Video_Processor:
             msg.exec()
             return
 
-        self.processor.load_video(self.video_path)
+        self.processor.load_video(self.video_path, self.brightness_value, self.contrast_value, self.sharpness_value, self.noise_value)
         self.processor.running = True  # если у тебя такой флаг используется
 
         try:
@@ -289,54 +275,130 @@ class Ui_Video_Processor:
         self.timer.timeout.connect(self.update_processed_frame)
         self.timer.start(33)  # ≈30 FPS
 
-    def update_brightness(self, value):
-        self.brightness.setText(str(value))
-        self.brightness_value = value
+    def update_frame(self):
+        if not self.cap or self.current_frame_index >= self.total_frames:
+            return  # если нет видео или достигнут конец
 
-    def update_contrast(self, value):
-        self.contrast.setText(str(value))
-        self.contrast_value = value
+        # Читаем кадр
+        self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.current_frame_index)
+        ret, frame = self.cap.read()
 
-    def update_sharpness(self, value):
-        self.sharpness.setText(str(value))
-        self.sharpness_value = value
+        if not ret:
+            return  # если кадр не был считан
 
-    def update_noise(self, value):
-        self.noise.setText(str(value))
-        self.noise_value = value
+        # Применяем фильтры (яркость, контраст, резкость, шум) на лету
+        frame = self.apply_filters(frame)
 
-    def set_default_slider_values(self):
-        self.brt_slide.setValue(0)
-        self.cntr_slide.setValue(1)
-        self.shrrp_slide.setValue(1)
-        self.noise_slide.setValue(1)
-
-        self.brightness.setText("0")
-        self.contrast.setText("1")
-        self.sharpness.setText("1")
-        self.noise.setText("1")
-
-    def update_processed_frame(self):
-        frame, mask = self.processor.process_frame()
         if frame is not None:
-            # frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             h, w, ch = frame.shape
             bytes_per_line = ch * w
             qt_img = QImage(frame.data, w, h, bytes_per_line, QImage.Format.Format_RGB888)
             pixmap = QPixmap.fromImage(qt_img)
             self.frame.setPixmap(pixmap)
-        if mask is not None:
-            qimg = QImage(mask.data, mask.shape[1], mask.shape[0], mask.shape[1], QImage.Format.Format_Grayscale8)
-            pixmap = QPixmap.fromImage(qimg)
-            self.mask.setPixmap(pixmap)
-        else:
+
+        self.update_time_label(self.current_frame_index)
+
+    def apply_filters(self, frame):
+        frame = cv2.convertScaleAbs(frame, alpha=self.contrast_value, beta=self.brightness_value)
+
+        # Применяем резкость (если значение больше 0)
+        shr = self.sharpness_value
+        if shr > 0:
+            base_kernel = np.array([[0, -1, 0],
+                                    [-1, 5 + shr * 2, -1],
+                                    [0, -1, 0]])
+            frame = cv2.filter2D(frame, -1, base_kernel)
+
+        # Применяем шум (если установлен флаг)
+        ns = self.noise_value
+        if ns > 0:
+            kernel_size = int(ns)
+            if kernel_size % 2 == 0:
+                kernel_size += 1
+            frame = cv2.medianBlur(frame, kernel_size)
+
+        return frame
+
+    def update_brightness(self, value):
+        self.brightness.setText(str(value))
+        self.brightness_value = value
+        self.update_frame()
+
+    def update_contrast(self, value):
+        self.contrast.setText(str(value))
+        self.contrast_value = value*0.5
+        self.update_frame()
+
+    def update_sharpness(self, value):
+        self.sharpness.setText(str(value))
+        self.sharpness_value = value*0.1
+        self.update_frame()
+
+    def update_noise(self, value):
+        self.noise.setText(str(value))
+        self.noise_value = value
+        self.update_frame()
+
+    def set_default_slider_values(self):
+        self.brt_slide.setValue(0)
+        self.cntr_slide.setValue(1)
+        self.shrrp_slide.setValue(0)
+        self.noise_slide.setValue(0)
+
+        self.brightness.setText("0")
+        self.contrast.setText("1")
+        self.sharpness.setText("0")
+        self.noise.setText("0")
+
+    # def update_processed_frame(self):
+    #     frame, mask = self.processor.process_frame()
+    #     if frame is not None:
+    #         # frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    #         h, w, ch = frame.shape
+    #         bytes_per_line = ch * w
+    #         qt_img = QImage(frame.data, w, h, bytes_per_line, QImage.Format.Format_RGB888)
+    #         pixmap = QPixmap.fromImage(qt_img)
+    #         self.frame.setPixmap(pixmap)
+    #     if mask is not None:
+    #         qimg = QImage(mask.data, mask.shape[1], mask.shape[0], mask.shape[1], QImage.Format.Format_Grayscale8)
+    #         pixmap = QPixmap.fromImage(qimg)
+    #         self.mask.setPixmap(pixmap)
+    #
+    #     else:
+    #         self.timer.stop()
+
+    def update_processed_frame(self):
+        if self.current_frame_index >= self.total_frames:
+            # Если видео закончилось, остановим таймер
+            self.timer.stop()
+            self.reset_video_state()  # Восстановить состояние после окончания видео
+            return
+
+        try:
+            frame, mask = self.processor.process_frame()  # Обработка кадра
+            if frame is not None:
+                h, w, ch = frame.shape
+                bytes_per_line = ch * w
+                qt_img = QImage(frame.data, w, h, bytes_per_line, QImage.Format.Format_RGB888)
+                pixmap = QPixmap.fromImage(qt_img)
+                self.frame.setPixmap(pixmap)
+
+            if mask is not None:
+                qimg = QImage(mask.data, mask.shape[1], mask.shape[0], mask.shape[1], QImage.Format.Format_Grayscale8)
+                pixmap = QPixmap.fromImage(qimg)
+                self.mask.setPixmap(pixmap)
+
+        except Exception as e:
+            print(f"Ошибка при обработке кадра: {e}")
             self.timer.stop()
 
+    def reset_video_state(self):
+        self.current_frame_index = 0
+        self.video_time.setText("00:00 / 00:00")
+        self.file.setText("Выбрать файл")
+        self.file.setStyleSheet(self.button_style_white)
+        self.set_default_slider_values()  # Восстановление значений слайдеров
+        self.processor.running = False  # Остановить обработку
 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    Video_Processor = QtWidgets.QMainWindow()
-    ui = Ui_Video_Processor()
-    ui.setupUi(Video_Processor)
-    Video_Processor.show()
-    sys.exit(app.exec())
+
